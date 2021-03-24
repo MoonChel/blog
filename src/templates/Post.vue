6ee8eebfb4d9c65fc5daa956bfcfa4db7a0f6152
Vladimir <newmoonchel@gmail.com>
2021-03-24 14:11:43 +0100

    Update Post.vue

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -3,7 +3,7 @@
     <br>
     <g-link to="/" class="link">  &larr; Go Back</g-link>
     <div class="post-title">
-      <h1>{{$page.post.title}}</h1>
+      <h1 v-html="$page.post.title"></h1>
       <p class="post-date"> {{ $page.post.date}} | {{$page.post.timeToRead}} min read</p>
     </div>
     <div class="post-content">
